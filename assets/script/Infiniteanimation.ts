b71216c03e5b1d1fccd91ed3c86dde960d97886e
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-16 12:53:06 +0530

    add promises but not working

--- assets/script/Infiniteanimation.ts
+++ assets/script/Infiniteanimation.ts
@@ -12,11 +12,71 @@ export class Infiniteanimation extends Component {
     removeCardNodes: Node[] = [];
     start() {
         this.createCards();
-        this.startInfiniteAnimation();
+        // let currentCardIndex = 0;
+
+        // const firstX = card.position.x;
+
+        // this.startInfiniteAnimation();
+        this.startInfiniteAnimation(0);
+    }
+
+    startInfiniteAnimation(currentCardIndex: number) {
+        console.log("startInfinite Animation");
+        const card = this.cardNodes[currentCardIndex];
+        this.node.addChild(card);
+        currentCardIndex = (currentCardIndex + 1) % this.cardNodes.length;
+        if (currentCardIndex % 2 == 0) {
+            this.changeScale(card)
+                .catch()
+                .then(() => {
+                    this.flipCard(card);
+                })
+                .catch()
+                .then(() => this.startInfiniteAnimation(currentCardIndex))
+                .catch();
+        } else {
+            this.changeScale(card)
+                .then(() => {
+                    this.changeCardFace(card, currentCardIndex);
+                })
+                .catch()
+                .then(() => this.flipCard(card))
+                .catch()
+                .then(() => this.startInfiniteAnimation(currentCardIndex))
+                .catch();
+        }
+    }
+    changeCardFace(card: Node, currentCardIndex: number) {
+        console.log("change card face");
+        return new Promise((resolve, reject) => {
+            resolve = () => {
+                if (currentCardIndex !== 0) {
+                    if (currentCardIndex % 2 == 0) card.getComponent(addCard).setCard();
+                }
+            };
+            reject = () => {
+                console.log("reject of change card face");
+            };
+        });
+    }
+
+    flipCard(card: Node) {
+        console.log("flip card");
+        return new Promise((resolve, reject) => {
+            resolve = () => {
+                tween(card)
+                    .to(0.4, { scale: v3(1.5, 1.5, 0) })
+                    .start();
+            };
+            reject = () => {
+                console.log("reject of flip card");
+            };
+        });
     }
 
     update(deltaTime: number) {}
     createCards() {
+        console.log("create cards");
         for (let i = 0; i < 7; i++) {
             const cardInstance = instantiate(this.cardPrefab);
             // this.node.addChild(cardInstance);
@@ -28,35 +88,50 @@ export class Infiniteanimation extends Component {
 
             this.cardNodes.push(cardInstance);
         }
+        console.log(this.cardNodes.length);
+    }
+    changeScale(card: Node) {
+        console.log("change scale");
+        return new Promise((resolve, reject) => {
+            resolve = () => {
+                tween(card)
+                    .to(0.4, { scale: v3(1.2, 1.2, 0) }, { easing: "backOut" })
+                    .to(0.4, { scale: v3(0.2, 0.2, 0) }, { easing: "backIn" })
+                    .start();
+            };
+            reject = () => {
+                console.log("reject of change scale");
+            };
+        });
     }
 
-    private startInfiniteAnimation() {
-        let currentCardIndex = 0;
+    // private startInfiniteAnimation() {
+    //     let currentCardIndex = 0;
 
-        const animateNextCard = () => {
-            const card = this.cardNodes[currentCardIndex];
-            this.node.addChild(card);
-            const firstX = card.position.x;
-            tween(card)
-                .to(0.4, { scale: v3(1.2, 1.2, 0) }, { easing: "backOut" })
-                .call(() => {
-                    console.log("call function called");
-                    currentCardIndex = (currentCardIndex + 1) % this.cardNodes.length;
-                    if (currentCardIndex !== 0) {
-                        if (currentCardIndex % 2 == 0) card.getComponent(addCard).setCard();
+    //     const animateNextCard = () => {
+    //         const card = this.cardNodes[currentCardIndex];
+    //         this.node.addChild(card);
+    //         const firstX = card.position.x;
+    //         tween(card)
+    //             .to(0.4, { scale: v3(1.2, 1.2, 0) }, { easing: "backOut" })
+    //             .call(() => {
+    //                 console.log("call function called");
+    //                 currentCardIndex = (currentCardIndex + 1) % this.cardNodes.length;
+    //                 if (currentCardIndex !== 0) {
+    //                     if (currentCardIndex % 2 == 0) card.getComponent(addCard).setCard();
 
-                        animateNextCard();
-                    }
-                })
+    //                     animateNextCard();
+    //                 }
+    //             })
 
-                .to(0.8, { scale: v3(0.2, 0.2, 0) }, { easing: "backOut" })
+    //             .to(0.8, { scale: v3(0.2, 0.2, 0) }, { easing: "backOut" })
 
-                .repeatForever()
-                .start();
-        };
+    //             .repeatForever()
+    //             .start();
+    //     };
 
-        animateNextCard();
-    }
+    //     animateNextCard();
+    // }
 
     // startInfiniteAnimation() {
     //     let currentCardIndex = 0;
@@ -79,14 +154,6 @@ export class Infiniteanimation extends Component {
     //     };
     //     animateNextCard();
     // }
-
-    changeScale(card: Node) {
-        return new Promise((resolve, reject) =>{
-            
-
-        })
-
-    }
 
     // comingAnimation(card: Node) {
     //     return new Promise
